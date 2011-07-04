c5f5e6b868cf3aa83b5e1de9459c66400cf1bc35
jimmeh <jimmy@ggps3>
2011-07-04 19:22:09 +0100

    ppc

--- new/lib/timing.h
+++ new/lib/timing.h
@@ -1,10 +1,18 @@
+#include <sys/time.h>
+
+
 /* timing code */
 unsigned long long get_time()
 {
- unsigned long lo,hi;
- __asm__ __volatile__("xorl %%eax,%%eax\n\
-cpuid\n\
-rdtsc" : "=a" (lo), "=d" (hi) : : "ebx", "ecx" );
- return ((unsigned long long)lo) + (unsigned long long)(hi<<32ULL);
+	unsigned long long res;
+
+	timeval t;
+
+	gettimeofday(&t, NULL);
+    res = t.tv_sec;
+	res *= (1000 * 1000);
+	res += t.tv_usec;
+
+	return res;
 }
 
--- new/lib/volatile_functions.h
+++ new/lib/volatile_functions.h
@@ -1,4 +1,4 @@
 #define vol_inc(x) asm volatile ("addl $1,%0":"=r"(x):"0"(x));
-#define fence() asm volatile ("mfence")
-#define pause() asm volatile ("pause")
-#define restorepr()
+#define fence() asm volatile ("sync")
+#define pause() asm volatile ("or 31,31,31")
+#define restorepr() asm volatile ("or 2,2,2");
